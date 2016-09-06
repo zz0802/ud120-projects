b301d4c71211ad8d1c2479aaf6ce0bde283c4d5a
zz0802 <zz820802@gmail.com>
2016-09-06 15:26:42 -0400

    completed dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,38 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+len(enron_data) # count number of person
 
+len(enron_data.values()[0])  # count features per key(person)
+
+count = 0               #count the number of POI 
+for i in enron_data.values():
+    if i['poi'] == True:
+        count += 1
+
+enron_data['PRENTICE JAMES']['total_stock_value']  #stock belong to ...
+enron_data['COLWELL WESLEY']['from_this_person_to_poi']  #
+enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+enron_data['SKILLING JEFFREY K']['total_payments']
+enron_data['FASTOW ANDREW S']['total_payments']
+enron_data['LAY KENNETH L']['total_payments']
+
+count = 0               #count the number of people do not have salary data
+for i in enron_data.values():
+    if i['salary'] != 'NaN':
+        count += 1
+
+count = 0               #count the number of people do not have email
+for i in enron_data.values():
+    if i['email_address'] != 'NaN':
+        count += 1
+
+count = 0               #count the number of people do not have email
+for i in enron_data.values():
+    if i['total_payments'] == 'NaN':
+        count += 1
+float(count)/len(enron_data)
+
+
+sorted(enron_data.keys())
